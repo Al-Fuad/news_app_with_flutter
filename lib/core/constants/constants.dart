462e114302c6b67e6e14bf90d7ccef8267b07e31
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-10-08 06:35:03 +0600

    Add constants for news API base URL, top headlines, API key, country, and category

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,5 @@
+const String newsAPIBaseURL = "https://newsapi.org/v2";
+const String topHeadline = "/top-headlines";
+const String apiKey = "9d84b6ec61a74e99a390b9da6134d7fa";
+const String country = "us";
+const String category = "general";
